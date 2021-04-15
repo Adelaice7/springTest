48c613034e89b34edd280dca3c305b1962dcf49a
Ramona Meunier <gramyy7@gmail.com>
2021-04-15 18:49:42 +0200

    basic MVC, templates

---
+++ src/main/java/com/adelaice/springtest/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.adelaice.springtest.controller;
+
+import com.adelaice.springtest.repo.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
